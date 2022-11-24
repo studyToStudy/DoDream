7a68bf6dd9f1c317f310715efeb272c14850ee05
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-11-24 15:45:27 +0900

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by 이정후 on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
