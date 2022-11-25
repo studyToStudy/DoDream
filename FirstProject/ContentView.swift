86ec25cd6fe8ea7d7fb3afdb7be6abb7b220110b
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-11-26 00:40:14 +0900

    project init

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text("Turtle Rock")
+            .font(.title)
+//            .foregroundColor(Color.green)
+            Text("Tree National Park")
         }
-        .padding()
     }
 }
 
