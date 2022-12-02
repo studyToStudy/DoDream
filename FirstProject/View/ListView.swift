65620ddf0b27485607aa19543cae664eb2684317
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-12-02 23:20:48 +0900

    add new view

---
+++ FirstProject/View/ListView.swift
@@ -0,0 +1,22 @@
+//
+//  ListView.swift
+//  FirstProject
+//
+//  Created by 이정후 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        List {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
